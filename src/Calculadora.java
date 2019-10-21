444a8c31514ac752e0c4758e34d6bef0d183f13f
´Jersson-Andrango´ <jersson059@hotmail.com>
2019-10-20 21:31:38 -0500

    Actualizacion

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,9 +7,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Calculadora  {
-JFrame principal = new JFrame ("Calculadora V1");
+JFrame principal = new JFrame ("Calculadora V2");
 JButton suma = new JButton ("Suma");
 JButton resta = new JButton ("Resta");
+JButton multiplicacion = new JButton ("Multiplicacion");
+JButton division = new JButton ("Division");
 JLabel valor1 = new JLabel ("Primer Valor");
 JLabel valor2 = new JLabel ("Segundo Valor");
 JLabel valor3 = new JLabel ("Resultado");
@@ -18,9 +20,6 @@ JLabel valor5 = new JLabel ("=");
 JTextField numero1 = new JTextField ();
 JTextField numero2 = new JTextField ();
 JTextField numero3 = new JTextField ();
-
-
-
 
 Calculadora() {   // Metodo constructor 
 	
@@ -46,6 +45,7 @@ Calculadora() {   // Metodo constructor
 	
 	principal.add(numero2);
 	numero2.setBounds(170, 100, 70, 30);
+	
 	principal.add(numero3);
 	numero3.setBounds(300, 100, 70, 30);
 	
@@ -54,38 +54,67 @@ Calculadora() {   // Metodo constructor
 	principal.add(suma);
 	suma.setBounds(50, 200, 70, 50);
 	suma.addActionListener(new suma());
+	
 	principal.add(resta);
 	resta.setBounds(150, 200, 70, 50);
 	resta.addActionListener(new resta());
 	
+	principal.add(multiplicacion);
+	multiplicacion.setBounds(250, 200, 70, 50);
+	multiplicacion.addActionListener(new multiplicacion());
+	
+	principal.add(division);
+	division.setBounds(350, 200, 70, 50);
+	division.addActionListener(new division());
 	
 }
 class suma implements ActionListener {
 	public void actionPerformed (ActionEvent e) {
 		
 		int num1, num2, suma;
-		
-
 			num1=Integer.parseInt (numero1.getText());
 			num2=Integer.parseInt (numero2.getText());
 			suma = num1 + num2;
 			numero3.setText(Integer.toString(suma));
-		
-
-
 		}
 }
 
 class resta implements ActionListener {
 	public void actionPerformed (ActionEvent e) {
 		
-		int num1, num2, resta;
-		
+		int num1, num2, resta;	
 
 			num1=Integer.parseInt (numero1.getText());
 			num2=Integer.parseInt (numero2.getText());
 			resta = num1 - num2;
 			numero3.setText(Integer.toString(resta));
+			
+
+		}
+}
+class division implements ActionListener {
+	public void actionPerformed (ActionEvent e) {
+		
+		int num1, num2, division;	
+
+			num1=Integer.parseInt (numero1.getText());
+			num2=Integer.parseInt (numero2.getText());
+			division = num1 / num2;
+			numero3.setText(Integer.toString(division));
+			
+
+		}
+}
+
+class multiplicacion implements ActionListener {
+	public void actionPerformed (ActionEvent e) {
+		
+		int num1, num2, multiplicacion;	
+
+			num1=Integer.parseInt (numero1.getText());
+			num2=Integer.parseInt (numero2.getText());
+			multiplicacion = num1 * num2;
+			numero3.setText(Integer.toString(multiplicacion));
 			
 
 		}
